cb6de9fc15c6d1902036c48ec5dbb3f7f52fa70f
Shreyas Uday <udayshreyas123456@gmail.com>
2025-12-15 21:59:05 +0530

    docs: update README and fix Supabase db config

--- db.js
+++ db.js
@@ -3,14 +3,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = new pg.Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-});
+// const db = new pg.Client({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+// });
 
-db.connect();
+// db.connect();
+const db = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+});
 
 export default db;
